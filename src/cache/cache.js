be3c291e0ec926e77e2917110e0a71c16decb006
Artur Kot <arturkot@onmail.com>
2023-08-01 12:43:29 +0200

    refactor: remove js files

--- src/cache/cache.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cache = void 0;
-var LRUCache_1 = require("./LRUCache");
-var cache = new LRUCache_1.LRUCache(1000);
-exports.cache = cache;
